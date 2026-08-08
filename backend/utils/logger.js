1778daea2a7e3472afd98fd2f383c23a17c99cf5
Shahriar Pavel <mdpavel6400@gmail.com>
2026-08-09 00:29:03 +0600

    feat: implement PLG stack

---
+++ backend/utils/logger.js
@@ -0,0 +1,19 @@
+import winston from "winston";
+
+const { combine, timestamp, json, errors } = winston.format;
+
+// Create a Winston logger that outputs structured JSON.
+// This JSON will be picked up by Promtail and sent to Loki.
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || "info",
+  format: combine(
+    errors({ stack: true }), // Ensure stack traces are included for errors
+    timestamp(),
+    json() // Output as pure JSON string on each line
+  ),
+  transports: [
+    new winston.transports.Console()
+  ],
+});
+
+export default logger;
